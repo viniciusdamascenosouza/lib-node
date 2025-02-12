edfcf4a9b57dad2a5bd7e470e074edc45ce43e3b
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2025-02-12 19:13:21 -0300

    capturing words

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ const fs = require("fs");
 const filePath = process.argv;
 const link = filePath[2];
 
+function checkDuplicatedWords(text) {
+  const wordsList = text.split(" ");
+  const result = {};
+  // object[property] = value;
+  wordsList.forEach((word) => {
+    result[word] = (result[word] || 0) + 1;
+  });
+  console.log(result);
+}
+
 fs.readFile(link, "utf-8", (_, text) => {
-  console.log(text);
+  checkDuplicatedWords(text);
 });
+
+// {
+//   web: 5,
+//   computador: 4,
+// }
