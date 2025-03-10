fa700efae5afb9897808647ccebab2bbf8b83192
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2025-03-10 19:50:26 -0300

    Identifying error types

--- src/index.js
+++ src/index.js
@@ -1,38 +1,41 @@
-const fs = require('fs'); 
+const fs = require("fs");
 
 const filePath = process.argv;
-const link = filePath[2]; 
+const link = filePath[2];
 
-fs.readFile(link, 'utf-8', (error, text) => {
+fs.readFile(link, "utf-8", (error, text) => {
   if (error) {
-    console.error(error); 
+    console.log("What is the error?", error.code);
     return;
   }
-  breakIntoParagraphs(text);
-  checkDuplicateWords(text);
-})
+  countsWords(text);
+});
 
-function breakIntoParagraphs(text) {
-  const paragraphs = text.toLowerCase().split('\n');
+function countsWords(text) {
+  const paragraphs = extractParagraphs(text);
   const count = paragraphs.flatMap((paragraph) => {
     if (!paragraph) return [];
     return checkDuplicateWords(paragraph);
-  })
+  });
   console.log(count);
 }
 
+function extractParagraphs(text) {
+  return text.toLowerCase().split("\n");
+}
+
 function cleanWords(word) {
-  return word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+  return word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
 }
 
 function checkDuplicateWords(text) {
-  const wordList = text.split(' ');
+  const wordList = text.split(" ");
   const result = {};
-  wordList.forEach(word => {
+  wordList.forEach((word) => {
     if (word.length >= 3) {
       const cleanWord = cleanWords(word);
-      result[cleanWord] = (result[cleanWord] || 0) + 1
+      result[cleanWord] = (result[cleanWord] || 0) + 1;
     }
-  })
+  });
   return result;
 }
