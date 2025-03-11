1e8dd72fd64b21fdb44106b1416852df1e6ac9e3
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2025-03-11 18:59:53 -0300

    Throw, catch e try

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 const fs = require("fs");
 
-const filePath = process.argv;
-const link = filePath[2];
+const filePath = process.argv[2];
 
-fs.readFile(link, "utf-8", (error, text) => {
+fs.readFile(filePath, "utf-8", (error, text) => {
   if (error) {
-    console.log("What is the error?", error.code);
+    if (error.code === "ENOENT") {
+      console.log("File not found.");
+    } else {
+      console.log("An error occurred while reading the file.");
+    }
     return;
   }
+
   countsWords(text);
 });
 
 function countsWords(text) {
   const paragraphs = extractParagraphs(text);
-  const count = paragraphs.flatMap((paragraph) => {
-    if (!paragraph) return [];
-    return checkDuplicateWords(paragraph);
-  });
+  const count = checkDuplicateWords(paragraphs);
   console.log(count);
 }
 
@@ -28,14 +29,16 @@ function cleanWords(word) {
   return word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
 }
 
-function checkDuplicateWords(text) {
-  const wordList = text.split(" ");
+function checkDuplicateWords(paragraphs) {
   const result = {};
-  wordList.forEach((word) => {
-    if (word.length >= 3) {
-      const cleanWord = cleanWords(word);
-      result[cleanWord] = (result[cleanWord] || 0) + 1;
-    }
+  paragraphs.forEach((paragraph) => {
+    const wordList = paragraph.split(" ");
+    wordList.forEach((word) => {
+      if (word.length >= 3) {
+        const cleanWord = cleanWords(word);
+        result[cleanWord] = (result[cleanWord] || 0) + 1;
+      }
+    });
   });
   return result;
 }
